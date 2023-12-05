f17560c20073c02f6378f89169a6fb83556a381b
Arsenibadov <147938560+Arsenibadov@users.noreply.github.com>
2023-12-05 19:22:51 +0300

    Измененное  дз 12

---
+++ src/Author.java
@@ -0,0 +1,38 @@
+
+import java.util.Objects;
+
+public class Author {
+    private final String authorName;
+    private final String authorSurname;
+
+    public Author(String authorName, String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorName() {
+        return this.authorName;
+    }
+
+    public String getAuthorSurname() {
+        return this.authorSurname;
+    }
+
+    @Override
+    public String toString() {
+        return " Author name - " + this.authorName + ", author surname - " + this.authorSurname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass () != o.getClass ()) return false;
+        Author author = (Author) o;
+        return Objects.equals ( authorName, author.authorName ) && Objects.equals ( authorSurname, author.authorSurname );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash ( authorName, authorSurname );
+    }
+}
